cab81c968d14108f977664b26401696d80f7543c
ibluedove <bluedove@yeah.net>
2018-07-19 10:14:02 +0800

    ad remove

--- e-example/e-micro-service/e-example-ms-db/src/main/java/com/loy/MainDb.java
+++ e-example/e-micro-service/e-example-ms-db/src/main/java/com/loy/MainDb.java
@@ -1,9 +1,6 @@
 /*
- * Copyright   Loy Fu.付厚俊
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +16,6 @@ import org.h2.tools.Server;
 
 /**
  * 
- * @author Loy Fu qq群 540553957  http://www.17jee.com
  * @since 1.8
  * @version 3.0.0
  * 
